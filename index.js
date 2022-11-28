56c93c5e478c560a76c1fd30d0fc11eac1f3427c
Arturo Lopez Lopez <orutra971@hotmail.com>
2022-11-28 15:53:47 -0600

    Alow localhost in cors

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const dbConfig = require("./app/config/db.config");
 const app = express();
 
 var corsOptions = {
-  origin: "https://tic-tac-toe-back-9lnq.vercel.app"
+  origin:[ "https://tic-tac-toe-back-9lnq.vercel.app", 'localhost']
 };
 
 app.use(cors(corsOptions));
