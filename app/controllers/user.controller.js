d0f7c4bdf30c1e69a26abe596b18218a83b67415
Arturo Lopez Lopez <orutra971@hotmail.com>
2022-11-28 17:47:43 -0600

    Loggin get players

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -22,12 +22,16 @@ exports.getPlayers = async (req, res) => {
   User.find(
     {},
     (err, users) => {
+      console.log({err});
       if (err) {
         res.status(500).json({ message: err.message });
         return;
       }
-      
-      res.status(200).json(users.map((e) => ({_id: e._id, username: e.username, image: e.image})));
+      const _users = users.map((e) => ({_id: e._id, username: e.username, image: e.image}));
+
+      console.log({_users});
+
+      res.status(200).json(_users);
     }
   );
 };
